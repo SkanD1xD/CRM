0f83e8ec4b3381cc8d73c58bf6ef141a426f9f39
SkanD1xD <kapranovaroslav@gmail.com>
2024-12-05 13:31:40 +0300

    вывод клиентов на экран

--- front/index.js
+++ front/index.js
@@ -1,11 +1,7 @@
 const url = "http://localhost:3000/api/clients"
 const addBtn = document.getElementById("addBtn")
 const modal = document.getElementById("modal")
-
-
-
-
-
+const dataTable = document.getElementById("dataTable")
 addBtn.addEventListener('click', ()=>{
     //показать модальное окно
     modal.classList.remove("hidden")
@@ -37,17 +33,9 @@ addBtn.addEventListener('click', ()=>{
          "contacts" : []
      }
     //  console.log({user})
-
     postData(user)
-
-
-
 })
-
-
 })
-
-
 
 function postData(user) {
     fetch(url, {
@@ -58,7 +46,38 @@ function postData(user) {
             }
         })
         .then((response) => { return response.json() })
-        .then((body) => console.log(body))
+        .then((body) => { console.log(body); getData()}
+     
+    )
 }
+
+
+function getData(){
+    fetch(url)
+    .then((response) => { return response.json() })
+    .then((users) => {console.log(users);   renderClients(users)})
+    
+}
+
+function renderClients(users){
+    users.forEach((user)=>{
+        dataTable.innerHTML += 
+        ` <tr class="main__row">
+                <td class="main__hcol">${user.id}</td>
+                <td class="main__hcol">${user.surname}  ${user.name}  ${user.lastName}</td>
+                <td class="main__hcol">${user.createdAt}</td>
+                <td class="main__hcol">${user.updatedAt}</td>
+                <td class="main__hcol">${user.contacts}</td>
+                 <td class="main__hcol">
+
+                 <button data-id = "${user.id}"> Изменить  </button>
+                   <button  data-id = "${user.id}"> Удалить  </button>
+                 </td>
+            </tr>`
+    }
+)
+}
+
+getData()
 
 
